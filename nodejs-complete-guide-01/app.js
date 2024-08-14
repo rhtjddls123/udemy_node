c6b954031e4d90d91a1714009dc3ee483c2f1e01
unknown <jjjk0605@naver.com>
2024-08-15 03:01:38 +0900

    router분리

--- nodejs-complete-guide-01/app.js
+++ nodejs-complete-guide-01/app.js
@@ -1,9 +1,12 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
+const adminRouter = require("./routes/admin.js");
+const shopRouter = require("./routes/shop.js");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRouter);
+app.use(shopRouter);
 
-server.listen(3000);
+app.listen(3000);
