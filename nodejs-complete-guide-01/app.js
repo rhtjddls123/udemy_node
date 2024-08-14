568869f7b08e72cc9da34fb93081711b97d141dd
unknown <jjjk0605@naver.com>
2024-08-15 04:09:09 +0900

    html페이지로 변환

--- nodejs-complete-guide-01/app.js
+++ nodejs-complete-guide-01/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -10,9 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRouter);
 app.use(shopRouter);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
