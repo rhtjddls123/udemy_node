df3d16329992915021d37b6bf4be90e36b2148a8
unknown <jjjk0605@naver.com>
2024-08-16 17:47:13 +0900

    pug, handlebars, ejs 실습

--- nodejs-complete-guide-01/app.js
+++ nodejs-complete-guide-01/app.js
@@ -1,20 +1,24 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRouter = require("./routes/admin.js");
-const shopRouter = require("./routes/shop.js");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRouter);
-app.use(shopRouter);
-app.use(express.static(path.join(__dirname, "public")));
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
