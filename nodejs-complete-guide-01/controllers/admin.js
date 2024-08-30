08540abca7a20c2027a015eae346831423fedea1
unknown <jjjk0605@naver.com>
2024-08-30 23:35:42 +0900

    페이지 추가

--- nodejs-complete-guide-01/controllers/admin.js
+++ nodejs-complete-guide-01/controllers/admin.js
@@ -1,30 +1,24 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    path: "/admin/add-product"
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const product = new Product(req.body);
   product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: "Admin Products",
+      path: "/admin/products"
     });
   });
 };
